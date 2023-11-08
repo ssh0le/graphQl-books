c9426a20a82f2d307dbff3566b7268ef080d0bf8
Bo <omuqqz@gmail.com>
2023-11-08 19:07:55 +0300

    feat: add server

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+import { config } from "./config.js";
+import { resolvers } from "./resolvers.js";
+import { typeDefs } from "./schemas.js";
+
+const { port } = config;
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: {
+        port: port,
+    },
+});
+
+console.log(`Server launched at ${port} port. URL: ${url}`);
