c2ed4a28ee5f94b0bf7f07a48ea92c2faf10c49c
Bo <omuqqz@gmail.com>
2023-11-09 12:08:05 +0300

    feat: update server

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,3 +1,3 @@
 export const config = {
-    port: 4000,
-}
+  port: 4000,
+};
--- server/src/helpers.ts
+++ server/src/helpers.ts
@@ -1,13 +1,13 @@
 const maxRandomValue = 10000;
 
 export const findElementById = <T extends { id: string }>(
-    elements: T[],
-    id: string
+  elements: T[],
+  id: string,
 ): T | null => {
-    const foundedElement = elements.find((element) => element.id === id);
-    return foundedElement ?? null;
+  const foundedElement = elements.find((element) => element.id === id);
+  return foundedElement ?? null;
 };
 
 export const generateId = () => {
-    return String(Math.floor(Math.random() * maxRandomValue));
+  return String(Math.floor(Math.random() * maxRandomValue));
 };
