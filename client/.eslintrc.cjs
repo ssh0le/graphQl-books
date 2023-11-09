5506a0e099550cf21db9786141c3aeb89d126b0a
Bo <omuqqz@gmail.com>
2023-11-09 12:17:44 +0300

    feat: update root files, add dependencies

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -8,11 +8,51 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort', 'react', 'prettier'],
   rules: {
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^@?\\w'],
+          ['@/styles/\\w*.css$'],
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$', '@/', '@UI'],
+          ['^\\u0000', '^\\./\\w'],
+        ],
+      },
+    ],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
 };
+// module.exports = {
+//   root: true,
+//   env: { browser: true, es2020: true },
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:react-hooks/recommended',
+//   ],
+//   ignorePatterns: ['dist', '.eslintrc.cjs'],
+//   parser: '@typescript-eslint/parser',
+//   plugins: ['react-refresh', 'simple-import-sort', 'react', 'prettier'],
+//   rules: {
+//     'simple-import-sort/imports': [
+//       'error',
+//       {
+//         groups: [
+//           ['^@?\\w'],
+//           ['@/styles/\\w*.css$'],
+//           ['^\\.\\.(?!/?$)', '^\\.\\./?$', '@/', '@UI'],
+//           ['^\\u0000', '^\\./\\w'],
+//         ],
+//       },
+//     ],
+//     'react-refresh/only-export-components': [
+//       'warn',
+//       { allowConstantExport: true },
+//     ],
+//   },
+// };
